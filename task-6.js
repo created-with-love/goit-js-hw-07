a20c006e35491e066c218bce2a78185d15255ad9
Vladymyr Ivanchenko <vladymyr_ivanchenko@bigmir.net>
2020-09-21 23:37:08 +0300

    добавляет мелкие правки

--- task-6.js
+++ task-6.js
@@ -4,7 +4,7 @@ validationBtnEl.addEventListener('blur', onBlurCheck);
 const validBtnClassList = validationBtnEl.classList;
 
 function onBlurCheck(event) {
-  if (validationBtnEl.value.length == validationBtnEl.dataset.length) {
+  if (validationBtnEl.value.length === Number(validationBtnEl.dataset.length)) {
     validBtnClassList.add('valid');
     validBtnClassList.remove('invalid');
   } else {
